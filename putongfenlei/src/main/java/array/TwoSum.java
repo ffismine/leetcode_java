db16dd87712b9da16bcb5d7fe785331a7e02b579
xz <xzreal_dlut@163.com>
2021-09-22 15:54:18 +0800

    0922

--- putongfenlei/src/main/java/array/TwoSum.java
+++ putongfenlei/src/main/java/array/TwoSum.java
@@ -1,3 +1,5 @@
+package array;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -18,12 +20,11 @@ import java.util.Map;
  */
 public class TwoSum {
     public static void main(String[] args) {
-        int[] result = new Solution().twoSum(new int[]{2, 7, 11, 15}, 9);
+        int[] result = twoSum(new int[]{2, 7, 11, 15}, 9);
         System.out.println(Arrays.toString(result));
     }
-}
-class Solution{
-    public int[] twoSum(int[] nums, int target){
+
+    public static int[] twoSum(int[] nums, int target){
         Map<Integer, Integer> map = new HashMap<>();
         for(int i = 0; i< nums.length; i++) {
             if(map.containsKey(target - nums[i])){
@@ -33,7 +34,9 @@ class Solution{
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+
 }
+
 
 
 
